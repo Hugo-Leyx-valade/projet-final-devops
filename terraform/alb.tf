353020cb04bc7b11c728f67d295a69162160bc0a
Hugo Leyx-valade <hleyx-valade@hellowork.com>
2026-05-26 16:23:04 +0200

    feat/terraform : pushing EC2 to create two instances

---
+++ terraform/alb.tf
@@ -0,0 +1,62 @@
+resource "aws_lb" "main" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = aws_subnet.public[*].id
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name        = "${var.project_name}-alb"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "${var.project_name}-tg-app"
+  port        = var.app_port
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    path                = "/api/health"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name        = "${var.project_name}-tg-app"
+    Environment = var.environment
+  }
+}
+
+
+resource "aws_lb_target_group_attachment" "app" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.app.arn
+  target_id        = aws_instance.app[count.index].id
+  port             = var.app_port
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  tags = {
+    Name        = "${var.project_name}-listener-http"
+    Environment = var.environment
+  }
+}
