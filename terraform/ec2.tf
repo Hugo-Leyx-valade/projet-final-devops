e056cfc4b5d821ccd62506a4a253c046570d5bcb
Hugo Leyx-valade <hleyx-valade@hellowork.com>
2026-05-26 16:22:58 +0200

    feat/terraform : pushing EC2 to create two instances

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -40,6 +40,7 @@ resource "aws_instance" "app" {
   subnet_id              = aws_subnet.private_app[count.index].id
   vpc_security_group_ids = [aws_security_group.app.id]
   key_name               = aws_key_pair.app.key_name
+  iam_instance_profile   = aws_iam_instance_profile.app.name
 
   user_data = templatefile("${path.module}/user_data/app.sh", {
     db_host     = aws_db_instance.main.address
